01d7c435cbdf085de287fb4a0a8d478c2dfd2e8f
alex <cyy0523xc@gmail.com>
2019-12-21 17:09:47 +0800

    增加距离矩阵

--- ClusterAlgo/optics.py
+++ ClusterAlgo/optics.py
@@ -9,14 +9,15 @@ import numpy as np
 
 def euclidean(p1, p2):
     """欧氏距离"""
-    return np.linalg.norm(p1 - p2)
+    return np.linalg.norm(p1.data - p2.data)
 
 
 class Point:
-    def __init__(self, data):
+    def __init__(self, index, data):
         self.data = data
         self.cd = None  # 核心距离
         self.rd = None  # 可达距离
+        self.index = index      # 索引
         self.processed = False  # 该样本点是否已经处理过
 
     def __repr__(self):
@@ -26,17 +27,33 @@ class Point:
 class Optics:
     inf = float('infinity')
 
-    def __init__(self, max_radius, min_cluster_size, distance=euclidean):
+    def __init__(self, max_radius, min_cluster_size,
+                 distance=None, matrix=None):
         """
         :param max_radius: int|float, 邻域半径
         :param min_cluster_size: int, 最小聚类的数据点的数量
         :param distance: function, 距离函数，可以在外部自定义，默认为欧氏距离
+        :param matrix: numpy.ndarray, 距离矩阵
         说明：
         距离函数定义：def function_name(point1, point2)
         """
         self.max_radius = max_radius
         self.min_cluster_size = min_cluster_size
-        self.distance = distance
+
+        # 初始化距离参数
+        if distance is not None:
+            self.distance = distance
+        elif matrix is not None:
+            assert type(matrix) == np.ndarray
+            self.matrix = matrix
+            self.distance = self.matrix_dist
+        else:
+            # 这两个参数不能全为None
+            self.distance = euclidean
+
+    def matrix_dist(self, p1, p2):
+        """计算两点之间的距离"""
+        return self.matrix[p1.index, p2.index]
 
     def _core_distance(self, point, neighbors):
         # distance from a point to its nth neighbor (n = min_cluser_size)
@@ -44,7 +61,7 @@ class Optics:
             return point.cd
         if len(neighbors) < self.min_cluster_size - 1:
             return None
-        sorted_neighbors = sorted([self.distance(point.data, n.data)
+        sorted_neighbors = sorted([self.distance(point, n)
                                    for n in neighbors])
         point.cd = sorted_neighbors[self.min_cluster_size - 2]
         return point.cd
@@ -52,7 +69,7 @@ class Optics:
     def _neighbors(self, point):
         """找到其所有直接密度可达样本点"""
         return [p for p in self.points if p is not point and
-                self.distance(point.data, p.data) <= self.max_radius]
+                self.distance(point, p) <= self.max_radius]
 
     def _processed(self, point):
         """ mark a point as processed """
@@ -67,7 +84,7 @@ class Optics:
             # find new reachability distance new_rd
             # if rd is null, keep new_rd and add n to the seed list
             # otherwise if new_rd < old rd, update rd
-            new_rd = max(point.cd, self.distance(n.data, point.data))
+            new_rd = max(point.cd, self.distance(n, point))
             if n.rd is None:
                 n.rd = new_rd
                 seeds.append(n)
@@ -75,7 +92,7 @@ class Optics:
                 n.rd = new_rd
 
     def fit(self, points):
-        self.points = [Point(row) for row in points]
+        self.points = [Point(i, row) for i, row in enumerate(points)]
 
         # 待处理队列
         self.unprocessed = [p for p in self.points]
@@ -137,14 +154,28 @@ if __name__ == "__main__":
         np.array((5, 7)),
     ]
 
+    # 使用欧氏距离
     optics = Optics(4, 2)
     optics.fit(points)
     clusters = optics.cluster(2)
-
     """
     输出：
     [[1 1], [2 2], [1 3]]
     [[4 6], [5 7]]
     """
+    for cluster in clusters:
+        print(cluster)
+
+    # 构造距离矩阵
+    n = len(points)
+    matrix = np.full((n, n), 0.0)
+    for i in range(n-1):
+        for j in range(i, n):
+            d = np.linalg.norm(points[i] - points[j])
+            matrix[i, j], matrix[j, i] = d, d
+
+    optics = Optics(4, 2, matrix=matrix)
+    optics.fit(points)
+    clusters = optics.cluster(2)
     for cluster in clusters:
         print(cluster)
